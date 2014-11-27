c91848e5e8e776d723e64d23855b6041d99ddad3
Ryan Tulino <ryan@inverseparadox.net>
2014-11-27 08:58:01 -0500

    update defaults

--- app/config/defaults.php
+++ app/config/defaults.php
@@ -15,7 +15,7 @@ return [
         'label'    => null,
         'type'     => 'varchar',
         'parent'   => false,
-        'default'  => null, // defaults must be less than 50 char
+        'default'  => null,
         'nullable' => true,
         'unique'   => false,
 
@@ -26,23 +26,15 @@ return [
     ],
 
     'layout' => [
-//        'callback' => null,
-//        'layout' => null,
-//        'script' => null,
-//        'style' => null,
-//        'meta' => null,
-//        'attributes' => null,
-//        'workflow' => null,
         'layout' => null,
         'position' => 0,
         'mode'  => Lavender::LAYOUT_APPEND
     ],
 
     'route' => [
-        'controller' => null, //'Lavender\Cms\DefaultController',
-        'action'     => null, //'getIndex',
-        'layout'     => null, //'default',
-        'workflow'   => null, //'default',
+        'controller' => null,
+        'action'     => null,
+        'layout'     => null,
         'method'     => 'get',
         'filter'     => null,
         'with'       => []
@@ -56,61 +48,10 @@ return [
     ],
 
     'workflow' => [
-        'view' => '', //container html where payloads are injected
-        'filters' => [], //bool callbacks run during each step
-        'states' => [
-            'my_step1' => [
-
-                // filter callbacks
-                'before_load' => [
-                    // todo prepare fields
-                    // todo inject defaults or session data
-                ],
-
-                // each payload is rendered into a form
-                'payloads' => [
-                    'my_custom_payload' => [
-                        'my_custom_key' => [
-                            'field_type' => 'some.key',//registered form field renderer & view
-                            'validate' => ['some.key','another.key'],//registered validation objects (php and/or js)
-                            'position' => null,
-                            'group' => null,
-                            'before_html' => null,
-                            'after_html' => null,
-                            'label' => null,
-                            // todo more stuff...
-                        ],
-                        'my_custom_key1' => [
-                            //...
-                        ],
-                    ],
-                    'my_custom_payload1' => [
-                        //...
-                    ]
-                ],
-
-                // filter fields
-                'before_save' => [],
-
-                // filter fields
-                'after_save' => [],
-
-                // filter callbacks w/ payload object(s)
-                'after_load' => [
-                    // todo throw FieldException to display inline field error
-                    // todo throw FormException to display payload error
-                    // todo throw Exception to display page error
-                ],
-
-            ],
-            'my_step2' => [
-                //...
-            ],
-        ],
-
-
-
-    ]
+        'transitions' => [],
+        'callbacks' => [],
+        'states' => [],
+    ],
 
 
 ];
