7dd9bcac9b8bd9a4014bb0b8a6b4e5e526ef819a
Ryan Tulino <ryan@inverseparadox.net>
2014-12-12 17:37:57 -0500

    add some default values!

--- app/config/store.php
+++ app/config/store.php
@@ -4,6 +4,12 @@ return [
 
     'default' => [
 
+        'name' => 'Lavender Commerce',
+        'email' => 'support@lavendercommerce.com',
+        'phone' => '215 555 1212',
+        'address' => "123 Ecommerce Ave\nPhiladelphia, PA 19101",
+        'hours' => "9am-5pm Monday-Friday\n10am-3pm Sat & Sun",
+
         /*
         |--------------------------------------------------------------------------
         | Login Throttle
