da2549b5d7070d42993d9bcc277bb96aa9f63fab
Ryan Tulino <ryan@inverseparadox.net>
2015-01-22 23:54:16 -0500

    fix copyright

--- src/Page/Composers/CopyrightComposer.php
+++ src/Page/Composers/CopyrightComposer.php
@@ -4,9 +4,15 @@ namespace Lavender\Page\Composers;
 class CopyrightComposer
 {
 
+    private $lavender = "https://github.com/lavender/lavender";
+
     public function compose($view)
     {
-        $view->with('copyright', sprintf("&copy; %s Lavender, Inc.", date('Y')));
+        $view->with('copyright', sprintf(
+            "&copy; %s  <a href=\"%s\">Lavender Commerce</a>",
+            date('Y'),
+            $this->lavender
+        ));
     }
 
 }
