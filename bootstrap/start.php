d8a655ee5624da52c66029d5b1c07153b58c5e67
Ryan Tulino <ryan@inverseparadox.net>
2015-01-25 13:02:00 -0500

    update session/cache to use file for testing env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'development' => [gethostname()],
+    'testing' => [gethostname()],
     'production' => [],
 
 ));
