d6ef8058f4442548b39fbad79954944ab455af66
Ryan Tulino <ryan@inverseparadox.net>
2015-02-21 19:36:25 -0500

    laravel 5 refactor

--- config/view.php
+++ config/view.php
@@ -2,33 +2,33 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | View Storage Paths
-    |--------------------------------------------------------------------------
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views. Of course
-    | the usual Laravel view path has already been registered for you.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| View Storage Paths
+	|--------------------------------------------------------------------------
+	|
+	| Most templating systems load templates from disk. Here you may specify
+	| an array of paths that should be checked for your views. Of course
+	| the usual Laravel view path has already been registered for you.
+	|
+	*/
 
-    'paths'      => [
-        __DIR__ . '/../views/'.App::environment(),
-        __DIR__ . '/../views/core'
-    ],
+	'paths' => [
+		realpath(base_path('resources/views/'.app()->environment())),
+		realpath(base_path('resources/views/core')),
+	],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Pagination View
-    |--------------------------------------------------------------------------
-    |
-    | This view will be used to render the pagination link output, and can
-    | be easily customized here to show any view you like. A clean view
-    | compatible with Twitter's Bootstrap is given to you by default.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Compiled View Path
+	|--------------------------------------------------------------------------
+	|
+	| This option determines where all the compiled Blade templates will be
+	| stored for your application. Typically, this is within the storage
+	| directory. However, as usual, you are free to change this value.
+	|
+	*/
 
-    'pagination' => 'pagination::slider-3',
+	'compiled' => realpath(storage_path().'/framework/views'),
 
 ];
