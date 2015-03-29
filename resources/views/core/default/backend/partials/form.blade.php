9a69b3e22f79b0856915c914adfddc9ffac154d4
Ryan Tulino <ryan@inverseparadox.net>
2015-03-29 10:41:15 -0400

    fixes printArray error in backend/config

--- resources/views/core/default/backend/partials/form.blade.php
+++ resources/views/core/default/backend/partials/form.blade.php
@@ -3,7 +3,13 @@
 
     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
-    @printArray($fields, "<div class='field'>", "</div>")
+    @foreach($fields as $field)
+    <div class='field'>
+
+        {!! $field !!}
+
+    </div>
+    @endforeach
 
 </form>
 
