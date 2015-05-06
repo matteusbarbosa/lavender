550b0aab0c62ec2d834c7fca189da0aa976ef251
ryan <ryan@inverseparadox.net>
2015-05-06 18:35:34 -0400

    clean up sample data

--- database/seeds/SampleData.php
+++ database/seeds/SampleData.php
@@ -4,24 +4,35 @@ use Illuminate\Database\Seeder;
 class SampleData extends Seeder
 {
 
+    public function __construct(\App\Store $store)
+    {
+        $this->store = $store;
+    }
+
     public function run()
     {
+        $this->sample_catalog();
 
+        $this->sample_blog();
+    }
+
+    protected function sample_catalog()
+    {
         $data = [
             2 => [
-                'name' => 'Child Category 1',
+                'name'   => 'Child Category 1',
                 'parent' => ['category' => 1],
             ],
             3 => [
-                'name' => 'Child Category 2',
+                'name'   => 'Child Category 2',
                 'parent' => ['category' => 1],
             ],
             4 => [
-                'name' => 'Grandchild Category 1',
+                'name'   => 'Grandchild Category 1',
                 'parent' => ['category' => 3],
             ],
             5 => [
-                'name' => 'Grandchild Category 2',
+                'name'   => 'Grandchild Category 2',
                 'parent' => ['category' => 3],
             ],
         ];
@@ -36,55 +47,55 @@ class SampleData extends Seeder
 
         $data = [
             [
-                'name' => 'Test Product 1',
-                'sku' => 'test-1',
-                'price' => 8.99,
+                'name'       => 'Test Product 1',
+                'sku'        => 'test-1',
+                'price'      => 8.99,
                 'categories' => ['category' => [3, 5]]
             ],
             [
-                'name' => 'Test Product 2',
-                'sku' => 'test-2',
-                'price' => 9.99,
+                'name'          => 'Test Product 2',
+                'sku'           => 'test-2',
+                'price'         => 9.99,
                 'special_price' => 5.99,
-                'categories' => ['category' => [2, 5]]
+                'categories'    => ['category' => [2, 5]]
             ],
             [
-                'name' => 'Test Product 3',
-                'sku' => 'test-3',
-                'price' => 8.99,
+                'name'       => 'Test Product 3',
+                'sku'        => 'test-3',
+                'price'      => 8.99,
                 'categories' => ['category' => [3, 5]]
             ],
             [
-                'name' => 'Test Product 4',
-                'sku' => 'test-4',
-                'price' => 8.99,
+                'name'       => 'Test Product 4',
+                'sku'        => 'test-4',
+                'price'      => 8.99,
                 'categories' => ['category' => [3, 5]]
             ],
             [
-                'name' => 'Test Product 5',
-                'sku' => 'test-5',
-                'price' => 9.99,
+                'name'          => 'Test Product 5',
+                'sku'           => 'test-5',
+                'price'         => 9.99,
                 'special_price' => 5.99,
+                'categories'    => ['category' => [3, 5]]
+            ],
+            [
+                'name'       => 'Test Product 6',
+                'sku'        => 'test-6',
+                'price'      => 8.99,
                 'categories' => ['category' => [3, 5]]
             ],
             [
-                'name' => 'Test Product 6',
-                'sku' => 'test-6',
-                'price' => 8.99,
+                'name'       => 'Test Product 7',
+                'sku'        => 'test-7',
+                'price'      => 8.99,
                 'categories' => ['category' => [3, 5]]
             ],
             [
-                'name' => 'Test Product 7',
-                'sku' => 'test-7',
-                'price' => 8.99,
-                'categories' => ['category' => [3, 5]]
-            ],
-            [
-                'name' => 'Test Product 8',
-                'sku' => 'test-8',
-                'price' => 9.99,
+                'name'          => 'Test Product 8',
+                'sku'           => 'test-8',
+                'price'         => 9.99,
                 'special_price' => 5.99,
-                'categories' => ['category' => [3, 5]]
+                'categories'    => ['category' => [3, 5]]
             ],
         ];
 
@@ -104,4 +115,23 @@ class SampleData extends Seeder
 
         }
     }
+
+    protected function sample_blog()
+    {
+        // create store
+        $store = entity('store')->create([
+            'theme'         => ['theme' => $this->store->theme_id],
+            'root_category' => ['category' => $this->store->category_id],
+        ]);
+
+        // set the current store scope
+        $this->store->setStore($store);
+
+        // set config for this store
+        $config = entity('store_config')
+            ->fill(['key' => 'name', 'value' => 'Lavender Blog']);
+
+        $config->save();
+    }
+
 }
